5b6667528a7609b5187431506e21d16d3235edf1
Jordancjx <60123885+Jordancjx@users.noreply.github.com>
2023-10-06 00:07:53 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
+import os.path
+from bs4 import BeautifulSoup
+
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 
+# Selenium options (Chromedriver)
 options = webdriver.ChromeOptions()
 options.add_argument(f'user-agent={user_agent}')
-options.headless = True
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')  # Fresh browser
+options.add_argument('--blink-settings=imagesEnabled=false') # Disable images, faster loading
 # options.add_argument('--headless=new')
 options.page_load_strategy = 'none'
 chrome_path = ChromeDriverManager().install()
@@ -22,40 +28,98 @@ driver = Chrome(options=options, service=chrome_service)
 driver.implicitly_wait(5)
 
 job_data = []
+links = []
 
-job_title = 'chef'
+# Declare CSV File Name and Headings
+csvFileName = "indeed_jobs.csv"
+csvHeading = ['jobTitle', 'jobCompany', 'jobLocation', 'jobMeta', 'jobContent', 'url']
+
+if not os.path.exists(csvFileName):
+    with open(csvFileName, 'w', newline="") as newcsv:
+        writer = csv.DictWriter(newcsv, fieldnames=csvHeading)
+        writer.writeheader()
+
+
+job_title = 'software engineer'
 location = 'Singapore'
 url = "https://sg.indeed.com/jobs?q="+job_title+"&l="+location+""
 driver.get(url)
 driver.implicitly_wait(3)
-job_listings = driver.find_elements(By.CLASS_NAME, 'jcs-JobTitle')  # Customize the XPath as needed
-print(job_listings)
 
+pages = 50
 
-# Write the header row
-for listing in job_listings:
-    print(listing)
+# Retrieve links
+for i in range(pages):
+    job_listings = driver.find_elements(By.CLASS_NAME, 'jcs-JobTitle')  # Customize the XPath as needed
+    print(job_listings)
+    for listing in job_listings:
+        driver.implicitly_wait(15)
+        product_url = listing.get_attribute('href')
+        links.append(product_url)
+        print(links)
+    if driver.find_elements(By.XPATH, "//a[@data-testid='pagination-page-next']"):
+        element = driver.find_element(By.XPATH, "//a[@data-testid='pagination-page-next']")
+        driver.execute_script("arguments[0].click();", element)
+        print("clicking on next page")
+    else:  # If unable to click "next"
+        print("Could not click next page, reached last page. Scraping jobs now...")
+        break
+
+# Go thru links and scrape
+for link in links:
+    driver.get(link)
     driver.implicitly_wait(15)
-    product_url = listing.get_attribute('href')
-    driver.get(product_url)
-    driver.implicitly_wait(5)
-    revealed = driver.find_element(By.CLASS_NAME, "fastviewjob")
-    WebDriverWait(driver, timeout=2).until(lambda d: revealed.is_displayed())
-    driver.implicitly_wait(10)
-    title = driver.find_element(By.CLASS_NAME, 'jobsearch-JobInfoHeader-title').text.strip()
-    company = driver.find_element(By.XPATH, '//div[@data-testid="inlineHeader-companyName"]').text.strip()
-    # location = driver.find_element(By.XPATH, '//div[@data-testid="inlineHeader-companyLocation').text.strip()
-    description = driver.find_element(By.ID, 'jobDescriptionText').text.strip()
-    meta = driver.find_element(By.ID, 'jobDetailsSection').text.strip()
+
+    try:
+        title = driver.find_element(By.CLASS_NAME, 'jobsearch-JobInfoHeader-title-container').text.strip()
+    except:
+        try:
+            title = driver.find_element(By.CLASS_NAME, 'jobsearch-JobInfoHeader-title-container').text.strip()
+        except:
+            title = "Not Found"
+
+    try:
+        job_link = link
+    except:
+        link = "Not Found"
+
+    try:
+        company = driver.find_element(By.XPATH, '//div[@data-testid="inlineHeader-companyName"]').text.strip()
+    except:
+        company = "Not Found"
+
+    try:
+        location = driver.find_element(By.XPATH, '//div[@data-testid="inlineHeader-companyLocation"]').text.strip()
+    except:
+        location = "Not Found"
+
+    try:
+        description = driver.find_element(By.ID, 'jobDescriptionText').text.strip()
+    except:
+        description = "Not Found"
+
+    try:
+        meta = driver.find_element(By.ID, 'salaryInfoAndJobType').text.strip()
+    except:
+        meta = "Not Found"
+
+
     job_data.append({'Job Title': title, 'Tags': meta, 'Company': company, 'Location':location, 'Description':description})
-    print(f"Tags:{meta}\nTitle: {title}\nCompany: {company}\nLocation: {location}\n")
-    driver.execute_script("window.history.go(-1)")
+    print(f"\nTitle: {title}\nLink:{job_link}\nCompany:{company}\nMeta:{meta}\nDescription:{description}\nLocation:{location}\n")
     driver.implicitly_wait(3)
 
+    reviewDictEntry = {
+        'jobTitle': title,
+        'jobCompany': company,
+        'jobLocation': location,
+        'jobMeta': meta,
+        'url': driver.current_url,
+        'jobContent': description
+    }
 
+    with open(csvFileName, 'a', encoding='utf-8', newline='') as f:
+        w = csv.DictWriter(f, fieldnames=csvHeading)
+        w.writerow(reviewDictEntry)
 
 driver.quit()
 
-df = pd.DataFrame(job_data)
-csv_filename = 'job_data.csv'
-df.to_csv(csv_filename, index=False)
